69811d8256b85040db7d22722cd2c0ed882841d1
Sonu HS <138276530+SonuHS1428@users.noreply.github.com>
2024-02-22 14:45:33 +0530

    Update variables.js

--- javascript/variables.js
+++ javascript/variables.js
@@ -1,24 +1,78 @@
-//Using var(Only use var if you MUST support old browsers.)
-//Using let(Only use let if you can't use const)
-//Using const(Always use const if the value should not be changed)
+/*Using var(Only use var if you MUST support old browsers.)
+Using let(Only use let if you can't use const)
+Using const(Always use const if the value should not be changed)*/
 
-var x = 100;
+/*Scope(Scope is the accessibility during runtime of variables, functions, or objects in a particular part of your code.)
+    var:Variables declared with var are in the global scope(When we define a variable inside a function,we can only access the variable inside the function.)
+    let:Variables declared with let are in the block scope.
+    const:Variables declared with const are in the block scope.*/
+
+// Global scope
+var myVar = 1;//can be accessed from anywhere in the code, including inside and outside of functions.
+
+// Function scope
+function myFunction() {
+  var myVar = 2;// This variable is local to the function, so it can only be accessed from within the function.
+  console.log(myVar); // 2
+}
+
+// Block scope
+{
+  let myVar = 3;
+  console.log(myVar); // 3
+}
+
+console.log(myVar); // 1
+
+
+function f1()
+{
+    var a=10;
+}
+console.log(a)//an error as a is not defined
+
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+console.log(i);//an error as i is not defined 
+
+
+/*Hoisting (means that we can define a variable before its declaration.)
+    var:allowed
+    let:not allowed
+    const:not allowed*/
+x = 8; 
 console.log(x);
+var x;//there is no error and that we can declare var variables after defining them.
 
-const s = "String";
-console.log(s);
+x = 8; 
+console.log(x);
+let x;//get an error as let variables do not support hoisting.same for const
 
-s1 = "Sonu";
-s2 = "HS";
 
-s3 = s1 + " " + s2; //`+` is used to concatenate strings
-console.log(s3);
+/*Reassign the value
+    var:allowed
+    let:allowed
+    const:not allowed*/
 
-var1 = 100;
-var2 = 20;
+/*Redeclaration of the variable
+    var:allowed
+    let:not allowed
+    const:not allowed*/
+var v1 = 1;
+var v1 = 30;
+console.log(v1);//30,there is no error 
 
-var3 = var1 + var2; // `+` is used to add two numbers
-console.log(var3);
+let v1 = 1;
+let v1 = 30;
+console.log(v1);//get an error as the let and const variables do not allow you to redeclare them again.
+
+
+
+
+
+
+
 
 
 
