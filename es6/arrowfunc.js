8efbf3057ad0d94cede039cbaec628dd97bf14e7
Sonu HS <138276530+SonuHS1428@users.noreply.github.com>
2024-02-22 15:14:44 +0530

    Update arrowfunc.js

--- es6/arrowfunc.js
+++ es6/arrowfunc.js
@@ -18,3 +18,13 @@ const func2 = ( x, y, z = 30 ) => {
 } 
 func2( 10, 20 );
 
+/* In regular functions, the object that is called the function was represented by the this keyword. 
+   This object could be a window, document, button, or anything else in case of regular functions.
+
+   this is based on the enclosing lexical scope for arrow functions. In arrow functions, the standard variable lookup is used.
+   They go on to the enclosing scope if they are unable to identify it in the current scope.
+   
+   there is no binding of this with arrow function, rather than binding their own this, the arrow functions inherit the "this" from the parent scope.
+   This process is called lexical scoping. 
+*/
+
