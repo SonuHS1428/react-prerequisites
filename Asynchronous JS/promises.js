1466af3cfbe9c0e815a194cde793f986a1a15747
Sonu HS <138276530+SonuHS1428@users.noreply.github.com>
2024-03-12 13:06:48 +0530

    Update promises.js

--- Asynchronous JS/promises.js
+++ Asynchronous JS/promises.js
@@ -17,8 +17,14 @@ const promise=createOrder(cart)
 promise.then(function(orderId){
      proceedToPayment(oderId);
 });
-
-//data:undefined
+/* here create order API won't take a callback function but it will only take card details and returns as a promise
+whenever this line is executed create order API returns as a promise,a promise is nothing but you can assume it to be a empty object
+and we know that create order API is an asynchronous operation and we don't know how much time it takes to execute
+suppose after 5 or 10 seconds create order API will create an order and it will gives us order details and that will fill this undefined place
+now we will attach a callback function to this promise object we will use then function which is available over promise object
+once we have data inside a promise object the callback function that will attach to a promise object will be automatically called, 
+and it will called cb func only once hence we will have control of our program with us
+promises gives us this guarantee that it will call callback function whenever there is a data inside the promise object*/
 /*In first code we PASSES CB FUNCTION to createOder api and blindly trust that api
   In second code we are ATTACHING A CB FUNCTION to a promise obj */
 
@@ -48,5 +54,52 @@ createOrder(cart)
     .then(function(paymentInfo){
         return updateWallet(paymentInfo);
     });
-    
 
+/*PROMISE APIs
+   if you make 10 parallel API calls and get the results for those parallel API calls there we will use this promise methods*/
+
+/*Promise.all()
+  Promise.allSettled()
+  Promise.race()
+  Promise.any()*/
+
+/*Promise.all():
+  it takes array of promises as input,let assume you have three promises P1, P2 ,P3 it will make 3 API calls parallelly and get the result
+  suppose all the API calls are success in that case the output of promise.all() will be array with a result of all three promises
+  as soon as any of these promise get rejected promise.all() will throw an error
+        all promises are success:         Promise.all([p1,p2,p3])------->[val1,val2,val3]
+        any of promise get reject:        Promise.all([p1,p2,p3])------->error */
+
+/* Promise.allSettled():
+ even though one of the API throws an error and if you want other two APIs result it can be done by using promise.allSettled()
+ what if P2 to get rejected still it will wait for all promises to settled
+        if promise p2 get rejected:        Promise.all([p1,p2,p3])------->[val1,err2,val3]*/
+
+/* Promise.race():
+   whether the promise is success or failure Promise.race() returns the result of first settled promise there itself
+   suppose p1,p2,p3 takes 3s,5s,2s to settled and assume p2 will get rejected..the output will be first it prints err2 then val3 then val1*/
+
+/* Promise.any():
+promise.any() again take list of promises and it is same as race the only difference is it will wait for the first promise to get successful
+in Promise.race() irrespective of success or failure it will return the result. over here you will wait for first success
+if all the promises get failed the returned result will be aggregated error which means all the promises are failed */
+
+const p1=new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve("p1 success"),3000);
+  });
+  const p2=new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve("p2 success"),1000);
+    //setTimeout(()=>reject("p2 fail"),1000);
+  });
+  const p3=new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve("p3 success"),2000);
+  });
+  Promise.all([p1,p2,p3])
+     .then((res)=>{console.log(res);
+      })
+     .catch((err)=>{
+        console.error(err);
+   });
+
+/*OUTPUT
+["p1 success","p2 success","p3 success"*/
