50931139952ec7de33f0d2ca7e9508e02d8b21b2
Sonu HS <138276530+SonuHS1428@users.noreply.github.com>
2024-03-12 21:02:34 +0530

    Update promises.js

--- Asynchronous JS/promises.js
+++ Asynchronous JS/promises.js
@@ -17,11 +17,11 @@ const promise=createOrder(cart)
 promise.then(function(orderId){
      proceedToPayment(oderId);
 });
-/* here create order API won't take a callback function but it will only take card details and returns as a promise
-whenever this line is executed create order API returns as a promise,a promise is nothing but you can assume it to be a empty object
+/* here createOrder API won't take a callback function but it will only take card details and returns as a promise
+whenever this line is executed createOrder API returns as a promise,a promise is nothing but you can assume it to be a empty object
 and we know that create order API is an asynchronous operation and we don't know how much time it takes to execute
-suppose after 5 or 10 seconds create order API will create an order and it will gives us order details and that will fill this undefined place
-now we will attach a callback function to this promise object we will use then function which is available over promise object
+suppose after 5 or 10 seconds createOrder API will create an order and it will gives us order details and that will fill this undefined place
+now we will attach a callback function to this promise object we will use then() function which is available over promise object
 once we have data inside a promise object the callback function that will attach to a promise object will be automatically called, 
 and it will called cb func only once hence we will have control of our program with us
 promises gives us this guarantee that it will call callback function whenever there is a data inside the promise object*/
@@ -77,11 +77,11 @@ createOrder(cart)
 
 /* Promise.race():
    whether the promise is success or failure Promise.race() returns the result of first settled promise there itself
-   suppose p1,p2,p3 takes 3s,5s,2s to settled and assume p2 will get rejected..the output will be first it prints err2 then val3 then val1*/
+   suppose p1,p2,p3 takes 3s,5s,2s to settled and assume p3 will get rejected..the output will be first it prints err3 then val1 then val2*/
 
 /* Promise.any():
 promise.any() again take list of promises and it is same as race the only difference is it will wait for the first promise to get successful
-in Promise.race() irrespective of success or failure it will return the result. over here you will wait for first success
+in Promise.race() irrespective of success or failure it will return the result. over here it will wait for first success
 if all the promises get failed the returned result will be aggregated error which means all the promises are failed */
 
 const p1=new Promise((resolve,reject)=>{
@@ -102,4 +102,4 @@ const p1=new Promise((resolve,reject)=>{
    });
 
 /*OUTPUT
-["p1 success","p2 success","p3 success"*/
+["p1 success","p2 success","p3 success"]*/
